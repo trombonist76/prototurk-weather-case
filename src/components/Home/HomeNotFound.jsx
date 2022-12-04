9466ccb07a3501a690d7098f90ead217ca03a3a5
Mahmut Hüdai <mgol@protel.com.tr>
2022-12-04 17:16:09 +0300

    [Add] HomeNotFound comp added

---
+++ src/components/Home/HomeNotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HomeNotFound(props) {
+  return (
+    <div className='flex flex-col items-center justify-center gap-6 text-rose-600 font-bold tracking-wider rounded-lg outline-offset-4 p-8 px-16 outline-dashed outline-slate-500 outline-2'>
+        Aradığınız kriterlere uygun bir şehir bulunamadı!
+        <button onClick={props.clearFilter} className='text-white text-sm font-medium p-2 px-4 bg-indigo-500 rounded-full hover:bg-indigo-300 transition-colors'>Filtreyi sıfırla</button>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,26 @@ import cities from "@/assets/data/cities.json";
 import HomeSwitch from "@/components/Home/HomeSwitch";
 import HomeSearch from "@/components/Home/HomeSearch";
 import HomeCity from "@/components/Home/HomeCity";
+import HomeNotFound from "@/components/Home/HomeNotFound";
 import { useMemo, useState } from "react";
 import { convertLocale } from "@/utils";
 
 export default function Home() {
   const [inputValue, setInputValue] = useState("");
-  const filteredCities = useMemo(() => cities.filter(city => convertLocale(city.name).includes(convertLocale(inputValue))), [inputValue])
+  const filteredCities = useMemo(
+    () =>
+      cities.filter((city) =>
+        convertLocale(city.name).includes(convertLocale(inputValue))
+      ),
+    [inputValue]
+  );
   const inputHandler = (event) => {
     setInputValue(event.target.value);
   };
+
+  const clearInput = () => {
+    setInputValue("")
+  }
   return (
     <div className="min-h-full flex flex-col items-center gap-12 bg-dark-light py-10">
       <h1 className="text-3xl font-bold tracking-wider">
@@ -18,15 +29,18 @@ export default function Home() {
       </h1>
       <HomeSwitch />
       <HomeSearch
-        className="w-1/3 bg-transparent border border-slate-500 rounded-sm p-2 outline-none"
+        className="w-1/3 bg-transparent border-2 border-slate-500 rounded-sm p-2 outline-none"
         value={inputValue}
         inputHandler={inputHandler}
       />
-      <div className="grid grid-cols-4 w-4/5 2xl:w-2/3 gap-3">
-        {filteredCities.map((city) => {
-          return <HomeCity city={city} key={city.id} />;
-        })}
-      </div>
+      {filteredCities.length > 0 
+        ? (
+          <div className="grid grid-cols-4 w-4/5 2xl:w-2/3 gap-3">
+            {filteredCities.map((city) => <HomeCity city={city} key={city.id} />)}
+          </div>) 
+        : (
+          <HomeNotFound clearFilter={clearInput}/>
+      )}
     </div>
   );
 }
