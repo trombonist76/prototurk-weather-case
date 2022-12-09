71cfc09a5ce3161a60027e7a56c56f99294549d8
Mahmut Hüdai <mgol@protel.com.tr>
2022-12-10 00:30:51 +0300

    [Add] React query provider added

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import router from "./router";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import './index.css'
+import { QueryClient, QueryClientProvider } from "react-query";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+    </QueryClientProvider>
   </Provider>
-)
+);
