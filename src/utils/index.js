796fee6e4d39b9a6b0da0e6a5fb1369e06a110a6
Mahmut Hüdai <mgol@protel.com.tr>
2022-12-12 01:25:12 +0300

    [Update] format date & group forecast data by day

--- src/utils/index.js
+++ src/utils/index.js
@@ -47,22 +47,68 @@ export function filterDataByTab(list, keyFunc) {
 }
 
 export function getHour(date) {
-  const parsedDate = dayjs(date).format("HH:mm");
+  const parsedDate = getFormattedDate(date, "hour")
   return parsedDate;
 }
 
 export function getHourAndDay(date) {
-  const parsedDate = dayjs(date).locale(tr).format("dddd - HH:mm")
+  const parsedDate = getFormattedDate(date, "dayHour")
   return parsedDate
 }
 
-export const capitalizeDescription = (description) => {
+function getFormattedDate(date, format) {
+  const formats = {
+    dayHour: "dddd - HH:mm",
+    hour: "HH:mm",
+    day: "dddd"
+  }
+
+  const parsedDate = dayjs(date).locale(tr).format(formats[format])
+  return parsedDate
+
+}
+
+export function capitalizeDescription(description) {
   const capitalized = description.split(" ").reduce((acc, cur) => {
     return `${acc} ${capitalizeFirstLetter(cur)}`
-  },"")
+  }, "")
 
   return capitalized
 }
 
-const capitalizeFirstLetter = ([ first, ...rest ], locale = "tr-TR") =>
+const capitalizeFirstLetter = ([first, ...rest], locale = "tr-TR") =>
   first === undefined ? '' : first.toLocaleUpperCase(locale) + rest.join('')
+
+export function getMinMaxFromDays(data) {
+  if (!Array.isArray(data)) return
+  const dates = data.reduce((groupedObj, currDate) => {
+    const day = getFormattedDate(currDate.dt_txt, "day")
+    const currWeather = currDate.main
+    
+    if (!groupedObj.hasOwnProperty(day)) {
+      groupedObj[day] = {
+        day,
+        min: currWeather.temp_min,
+        max: currWeather.temp_max,
+        iconId: currDate.weather.at(0).id,
+        date: currDate.dt
+      }
+      return groupedObj
+    }
+
+    const addedDate = groupedObj[day]
+    if (currWeather.temp_min < addedDate.min) addedDate.min = currWeather.temp_min
+    if (currWeather.temp_max > addedDate.max) addedDate.max = currWeather.temp_max
+
+    return groupedObj
+  }, {})
+
+  const sortedDates = sortByDate(dates)
+  return sortedDates
+}
+
+
+function sortByDate(dates) {
+  const sorted = Object.values(dates).sort((a, b) => a.date - b.date)
+  return sorted
+}
