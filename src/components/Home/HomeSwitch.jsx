d9d5619a0b3b69b3eeb619cae2b9c5619cba2a6e
Mahmut Hüdai <mgol@protel.com.tr>
2022-12-15 00:07:55 +0300

    [Update] Frosted-glass effect added on scroll

--- src/components/Home/HomeSwitch.jsx
+++ src/components/Home/HomeSwitch.jsx
@@ -1,8 +1,25 @@
 import classNames from "classnames";
+import useScrollPosition from "@/hooks/useScrollPosition";
+import { useEffect, useMemo, useRef, useState } from "react";
 
-export default function HomeSwitch(props) {
+export default function HomeSwitch(props, ref) {
+  const switchRef = useRef()
+  const [offsetTop, setOffsetTop] = useState()
+  const scrollPosition = useScrollPosition()
+  const isSwitchOnTop = useMemo(() => scrollPosition >= offsetTop, [scrollPosition])
+
+  useEffect(() => {
+    const {top, right, bottom, left} = switchRef.current.getBoundingClientRect()
+    setOffsetTop(top);
+  },[])
+
   return (
-    <div className="sticky top-20">
+    <div className={classNames(
+      "sticky top-0 p-5 w-full flex items-center justify-center transition-colors",{
+        "bg-slate-700 rounded-b-xl bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg": isSwitchOnTop
+      })} 
+      ref={switchRef}
+    >
       <button className="flex items-center justify-center gap-2 bg-dark p-2 rounded-full transition-colors">
         <span
           onClick={props.toggleLayout}
