dfb11e99f742af5235c7c30db9c9ae58cc2ff15c
Mahmut Hüdai <mgol@protel.com.tr>
2022-12-15 00:10:34 +0300

    [Update] Active card logic added

--- src/components/City/CityCard.jsx
+++ src/components/City/CityCard.jsx
@@ -1,10 +1,16 @@
 import IconWeather from "@/components/IconWeather"
+import classNames from "classnames"
 
 export default function CityCard(props) {
   const max = parseInt(props.max)
   const min = parseInt(props.min)
   return (
-    <div className='flex flex-col gap-4 items-center hover:bg-slate-700 p-6 rounded-md flex-1'>
+    <div onClick={props.setSelectedDay} className={classNames(
+      "flex flex-col gap-4 items-center hover:bg-slate-700 cursor-pointer p-6 rounded-md flex-1",
+      {
+        "bg-slate-700": props.isActive
+      }
+    )}>
         <span className='font-bold'>{props.day}</span>
         <IconWeather iconId={props.iconId}/>
         <div className='flex gap-2 text-sm'>
