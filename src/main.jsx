df85819416e6e0bbaa2739d535d5ac6ee52e18ce
trombonist76 <mahmut.hg@gmail.com>
2022-12-17 01:21:34 +0300

    [Delete] React-query removed

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,12 @@ import router from "./router";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
 import "./index.css";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <React.StrictMode>
-        <RouterProvider router={router} />
-      </React.StrictMode>
-    </QueryClientProvider>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   </Provider>
 );
